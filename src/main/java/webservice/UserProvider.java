c2e0f664a1809003817b6bb11a90ca793ecce830
mohammad shirzad <mshirzad70@gmail.com>
2018-10-08 22:32:48 +0330

    fixed an issue

--- src/main/java/webservice/UserProvider.java
+++ src/main/java/webservice/UserProvider.java
@@ -5,6 +5,7 @@ import model.entity.User;
 import model.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,11 +16,10 @@ public class UserProvider {
     @Autowired
     UserService userService;
 
-    @RequestMapping(value = "/save_user")
+    @RequestMapping(value = "/save_user", method = RequestMethod.POST)
     public int saveUser(@RequestParam(name = "user") String json) {
         try {
             Gson gson = new Gson();
-
             User user = gson.fromJson(json, User.class);
             return userService.saveEntity(user);
         } catch (Exception e) {
